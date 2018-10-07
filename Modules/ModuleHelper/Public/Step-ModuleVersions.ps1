e32b3e233cd50d7cbfeb4af4afd67c228d2da1dc
Casey <crouse.casey@gmail.com>
2018-10-07 16:18:44 -0700

    refactor(Scripts): Renamed Build-Modules.ps1 to Step-ModuleVersions.ps1
    Wrapped the code in the original Build-Modules script in a function named Step-ModuleVersions, then renamed the file to match the new function name.
    This fixes a #bug where the ModuleHelper module could not successfully import because the script did not contain any functions.

---
+++ Modules/ModuleHelper/Public/Step-ModuleVersions.ps1
@@ -0,0 +1,92 @@
+<#
+.SYNOPSIS
+    Increments the ModuleVersion property in all user PowerShell Module Manfiests
+.DESCRIPTION
+    Reads an existing Module Manifest file and increments the ModuleVersion property.
+.EXAMPLE
+    C:\PS> Step-ModuleVersions
+.LINK
+    http://semver.org/
+.LINK
+    New-ModuleManifest
+#>
+function Step-ModuleVersions()
+{
+    $script:UserModules  = @( Get-ChildItem -Recurse -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\*.psm1" -ErrorAction SilentlyContinue )
+
+    Write-Host "Updating module versions..." -ForegroundColor DarkBlue
+    Foreach($module in $UserModules)
+    {
+        $script:ModulePath = $module.FullName.Replace("$($module.name)", "");
+        $script:ManifestPath = $module.FullName.Replace("psm1", "psd1");
+        $script:ModuleFullName = $module.FullName;
+        $script:ModuleName = $module.Name.Replace(".psm1", "");
+        $script:Output = Join-Path -Path $ModulePath -ChildPath "Fingerprint";
+        $script:FingerprintPath = Join-Path -Path $Output -ChildPath "Fingerprint.txt";
+
+        Try
+        {
+            if (Test-Path "$ModulePath\Public")
+            {
+                $functions = Get-ChildItem "$ModulePath\Public\*.ps1" | Where-Object { $_.name -notmatch 'Tests'} | Select-Object -ExpandProperty basename
+                Set-ModuleFunctions -Name $ManifestPath -FunctionsToExport $functions
+            }
+
+            Write-Output "  Detecting semantic versioning"
+            Import-Module $ManifestPath
+            $commandList = Get-Command -Module $ModuleName
+            $version = $(Get-Module -Name $ModuleName).Version
+            Remove-Module $ModuleName
+
+            Write-Output "    Calculating fingerprint"
+            $fingerprint = foreach ($command in $commandList )
+            {
+                foreach ($parameter in $command.parameters.keys)
+                {
+                    '{0}:{1}' -f $command.name, $command.parameters[$parameter].Name
+                    $command.parameters[$parameter].aliases | Foreach-Object { '{0}:{1}' -f $command.name, $_}
+                }
+            }
+
+            if (Test-Path $FingerprintPath)
+            {
+                $oldFingerprint = Get-Content $FingerprintPath
+            }
+            else
+            {
+                New-Item -Path $Output -ItemType Directory
+                New-Item -Path $FingerprintPath -ItemType File
+            }
+
+            $bumpVersionType = 'Patch'
+            '    Detecting new features'
+            $fingerprint | Where-Object {$_ -notin $oldFingerprint } | % {$bumpVersionType = 'Minor'; "      $_"}
+            '    Detecting breaking changes'
+            $oldFingerprint | Where-Object {$_ -notin $fingerprint } | % {$bumpVersionType = 'Major'; "      $_"}
+
+            Set-Content -Path $FingerprintPath -Value $fingerprint -Force
+
+            # Bump the module version
+            if ($version -lt ([version]'1.0.0'))
+            {
+                # Still in beta, don't bump major version
+                if ($bumpVersionType -eq 'Major')
+                {
+                    $bumpVersionType = 'Minor'
+                }
+                else
+                {
+                    $bumpVersionType = 'Patch'
+                }
+            }
+
+            Write-Output "  Stepping [$bumpVersionType] version [$version]"
+            $version = [version] (Step-ModuleVersion -Path $ManifestPath -By $bumpVersionType)
+            Write-Output "  Using version: $version"
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import module $($import.fullname): $_"
+        }
+    }
+}
