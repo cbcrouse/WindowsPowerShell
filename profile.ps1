ee2ec88485d271cd0f1054586f85d221a8e57834
Casey <crouse.casey@gmail.com>
2018-10-07 13:26:19 -0700

    fix(Modules): Modules and Module Commands not auto-importing
    Modified master profile to load each module and then Get-Command so that `Get-Module -ListAvailable` shows each imported module and their exported commands.

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,20 @@
 Write-Host "CUAH Master Profile loaded." -ForegroundColor Yellow -BackgroundColor Black
+
+$Modules  = @( Get-ChildItem -Recurse -Path "$PSScriptRoot\Modules\*.psm1" -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Write-Host "Loading modules..." -ForegroundColor DarkBlue
+Foreach($import in $Modules)
+{
+    Try
+    {
+        Write-Host $import.Name -ForegroundColor DarkGreen
+        $manifestName = $import.fullname.Replace("psm1", "psd1")
+        Import-Module $manifestName
+        Get-Command -Module $import.name | Out-Null
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import module $($import.fullname): $_"
+    }
+}
