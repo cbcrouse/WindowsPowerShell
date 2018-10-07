d839e61e15d06de3a0c6d02053e1d078cc7a1649
Casey <crouse.casey@gmail.com>
2018-10-06 21:24:46 -0700

    fix(Profiles): Broken path and incorrect filename
    Fixed a #bug where Get-Location's return value was not valid for this context.
    The modules directory is now set using $env:USERPROFILE instead.

--- profile.ps1
+++ profile.ps1
@@ -3,11 +3,11 @@ Write-Host "CUAH Master Profile loaded." -ForegroundColor Yellow -BackgroundColo
 #######################################
 ## IMPORT MODULES
 #######################################
-$modulesDir = Join-Path -Path $(Get-Location) -ChildPath "Modules"
+$modulesDir = Join-Path -Path $($env:USERPROFILE) -ChildPath "Documents\WindowsPowerShell\Modules"
 $modules = Get-ChildItem -Path $modulesDir -Filter "*.psm1" -Recurse
 foreach($module in $modules)
 {
-    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($module)
+    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($module.FullName)
     Write-Host "Importing module: $fileName" -ForegroundColor DarkBlue
     Import-Module -Name $module
 }
